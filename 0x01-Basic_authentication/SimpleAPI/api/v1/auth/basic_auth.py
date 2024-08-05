c03731ad6e444a58a6b2261e426dd0f6fe936193
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-08-05 17:26:31 +0300

    Basic - Base64 part

--- 0x01-Basic_authentication/SimpleAPI/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/basic_auth.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python3
 """BasicAuth class definition"""
 
+import re
+
+
 class BasicAuth:
     """This class defines all its methods"""
-    pass
+    def __init__(self):
+        """initialization method"""
+        pass
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """This method extracts the base64 authorization header"""
+        if authorization_header:
+            if isinstance(authorization_header, str):
+                pattern = r'^Basic (.*)'
+                match = re.match(pattern, authorization_header)
+
+                if match:
+                    return match.group(1)
+
+        return None
