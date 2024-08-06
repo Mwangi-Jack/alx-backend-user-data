75d62bdf9afb12ac6ce3935a1d2d89ed712e304e
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-08-06 12:24:53 +0300

    added class inheritance to BasicAuth class

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,9 +3,10 @@
 
 import re
 import base64
+from auth import Auth
 
 
-class BasicAuth ():
+class BasicAuth (Auth):
     """This class defines all its methods"""
     def __init__(self):
         """initialization method"""
