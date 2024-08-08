2e86591e28ab293210eeb2dae4bdd0d2c0a0ccfc
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-08-08 13:23:29 +0300

    added another excluded path /auth_session/login

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -32,12 +32,21 @@ def before_request():
         request.current_user = auth.current_user(request)
         path = request.path
         excluded_paths = ['/api/v1/status/',
-                          '/api/v1/unauthorized/', '/api/v1/forbidden/']
+                          '/api/v1/unauthorized/',
+                          '/api/v1/forbidden/',
+                          '/api/v1/auth_session/login/'
+                          ]
         if auth.require_auth(path, excluded_paths):
             if auth.authorization_header(request) is None:
                 abort(401)
             if auth.current_user(request) is None:
                 abort(403)
+
+        if not auth.authorization_header(request):
+            return None
+
+        if not auth.session_cookie(request):
+            abort(401)
 
 
 @app.errorhandler(404)
