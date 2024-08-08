f9488408272d2700de821663e4b1bf0694680149
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-08-08 14:28:14 +0300

    added a new users route /users/me

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -43,11 +43,10 @@ def before_request():
             if auth.current_user(request) is None:
                 abort(403)
 
-        if auth.authorization_header(request) is None:
-            None
-
-        if auth.session_cookie(request) is None:
-            None
+        if auth.authorization_header(
+                request
+                ) is None and auth.session_cookie(request) is None:
+            return None, abort(401)
 
 
 @app.errorhandler(404)
