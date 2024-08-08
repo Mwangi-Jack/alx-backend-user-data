9324f15d6e715d4e80326829349edae7f0d8eb26
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-08-08 18:40:09 +0300

    modified before_request method

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -43,16 +43,11 @@ def before_request():
             if auth.current_user(request) is None:
                 abort(403)
 
-        # if auth.authorization_header(request) is None:
-        #     return None
+        if auth.authorization_header(request) is None:
+            return None
 
-        # if auth.session_cookie(request) is None:
-        #     abort(401)
-
-        if auth.authorization_header(
-            request
-            ) is None and auth.session_cookie(request) is None:
-            return None, abort(401)
+        if auth.session_cookie(request) is None:
+            abort(401)
 
 
 @app.errorhandler(404)
