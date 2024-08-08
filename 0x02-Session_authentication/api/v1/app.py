dc8dba970db355ca7fa26c3cdf2f1d5a18232a3a
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-08-08 14:31:24 +0300

    updated @app.before_request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -43,10 +43,8 @@ def before_request():
             if auth.current_user(request) is None:
                 abort(403)
 
-        if auth.authorization_header(
-                request
-                ) is None and auth.session_cookie(request) is None:
-            return None, abort(401)
+        if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
+            abort(401)
 
 
 @app.errorhandler(404)
