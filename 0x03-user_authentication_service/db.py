4af36ad7a975b8ea6aa91917938d521ef6b1dd0f
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-08-14 12:07:12 +0300

    created a db class and its methods

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -29,7 +29,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> Optional[User]:
+    def add_user(self, email: str, hashed_password: str) -> User:
         """This method returns a user object"""
 
         user = User(email=email, hashed_password=hashed_password)
