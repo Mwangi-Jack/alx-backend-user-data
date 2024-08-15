43d7de5b1860f9f906a01576642c5d5c71900f7d
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-08-15 13:41:56 +0300

    added a login route /sessions to app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -27,6 +27,7 @@ def users():
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
+
 @app.route('/sessions', methods=['POST'], strict_slashes=False)
 def login():
     """This mtehod defines the route to login a user"""
