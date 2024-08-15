b74f7d3b65b636d7760ef7920310d5b43a2eca76
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-08-15 11:58:46 +0300

    added users route to the app API to register a user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,15 +1,31 @@
 #!/usr/bin/env python3
 """This file defines all the API routes"""
 
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', strict_slashes=False)
 def index():
     """This function defines the index route"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users/', methods=['POST'], strict_slashes=False)
+def users():
+    """This function defines a route to register a user"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        user = AUTH.register_user(email, password)
+
+        return jsonify({"email": user.email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
