a46fb58562961e168d16061f1305e5df6ea45c04
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-08-15 11:09:05 +0300

    added register_user method to auth.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -52,11 +52,11 @@ class DB:
             user = query.one()
             return user
 
-        except NoResultFound:
-            raise NoResultFound
+        except NoResultFound as exc:
+            raise NoResultFound from exc
 
-        except InvalidRequestError:
-            raise InvalidRequestError
+        except InvalidRequestError as exc:
+            raise InvalidRequestError from exc
 
     def update_user(self, user_id: int, **kwargs: Dict[str, Any]) -> None:
         """
