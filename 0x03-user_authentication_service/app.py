32ee829c55884469498366ab228ebc602a1e33e1
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-08-15 15:40:07 +0300

    added an API route /profile to get user profile details

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -58,6 +58,19 @@ def logout() -> None:
     AUTH.destroy_session(user.id)
     return redirect(url_for('index'))
 
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile():
+    """This method defines the route to get user profile details"""
+
+    session_id = request.cookies.get('session_id')
+
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if user:
+        return jsonify({"email": user.email}), 200
+
+    abort(403)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
