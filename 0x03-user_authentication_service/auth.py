4bbac2bb8d65a668898f6e7ab987a47731b98fe0
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-08-15 16:01:16 +0300

    added a method get_reset_password_token to the Auth class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -104,3 +104,21 @@ class Auth:
 
         except NoResultFound:
             return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """
+        This method takes in a string argument 'email'
+        finds the user corresponding to the 'email' and returns
+        a uuid tokes if user exists else None
+        """
+
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+
+            self._db.update_user(user.id, reset_token=reset_token)
+
+            return reset_token
+
+        except NoResultFound as exc:
+            raise ValueError from exc
